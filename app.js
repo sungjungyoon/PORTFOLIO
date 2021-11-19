9c051a51776a66d25543a95470fcda3100e08c48
sungjungyoon <87048278+sungjungyoon@users.noreply.github.com>
2021-11-19 19:56:17 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// 전역변수
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+// 이벤트 토글
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+// 스크롤 내려올 때 색변경
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = 'rgb(0,0,.3)';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+// 햄버거 토글
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+// 스와이퍼 기능
+
+		var swiper = new Swiper(".swiper01", {
+			pagination: {
+			  el: ".swiper-pagination",
+			  clickable: true,
+			},
+			breakpoints: {
+				640: {
+				  slidesPerView: 1,
+				  spaceBetween: 20,
+				},
+				768: {
+				  slidesPerView: 1,
+				  spaceBetween: 20,
+				},
+				1200: {
+				  slidesPerView: 3,
+				  spaceBetween: 30,
+				},
+			},
+		  });
+
